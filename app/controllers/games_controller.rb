227ff275dff4d38c1c9eb4e95d9584b294b449eb
christel dufrasne <chbalsacq@gmail.com>
2020-08-11 10:55:50 +0200

    longest word game correction

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,32 +1,27 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+
   def new
     @letters = [*('A'..'Z')].sample(10).join
   end
 
+  def score
+    @letters = params[:letters].split
+    @word = (params [:word] || "").upcase
+    @include = include_or_not?(@word, @letters)
+    english_word = english_word?(word)
+  end
 
+ private
   def include_or_not(answer, letters)
     answer.split("").each do |letter|
     letters.include? letter
     end
   end
-
-  def score
-  @answer = params[:answer]
-  @letters = params[:ramdomletters]
-    if include_or_not(@answer.upcase, @letters)
-      ["well done !"]
-    else
-    "Sorry not in the random letters"
-      end
-    else
-      ["not an english word"]
-    end
-
-  end
-
-  def get_compar(answer)
-    response = open("http://api.wordreference.com/0.8/80143/json/enfr/#{answer.downcase}")
-    json = JSON.parse(response.read.to_s)
-    json['term0']['PrincipalTranslations']['0']['FirstTranslation']['term'] unless json["Error"]
+  def english_word?(answer)
+    response = open("https://wagon-dictionary.herokuapp.com/#{answer.downcase}")
+    json = JSON.parse(response.read)
+    return json['found']
   end
 end
